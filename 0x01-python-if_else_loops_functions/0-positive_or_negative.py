9e8f6785b1e506d5ac20d4c89f41bca6acdbc158
samuelsenerwa <samuelsenerwa@gmail.com>
2023-01-04 19:35:41 -0800

    added tasks

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,7 +3,6 @@ import random
 number = random.randint(-10, 10)
 if number > 0:
     print(f"{number:d} is positive")
-
 elif number == 0:
     print(f"{number:d} is zero")
 else:
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# Author - Godswill Kalu
 
 """Print numbers 0 to 98 in decimal and hexadecimal."""
 for number in range(0, 99):
