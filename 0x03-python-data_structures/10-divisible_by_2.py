d1ebba68f252907d23071c75c2f3c5d1d74825d3
samuelsenerwa <samuelsenerwa@gmail.com>
2023-01-09 22:16:10 -0800

    added a task

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for i in range(len(my_list)):
-        if my_list[i] % 2 == 0
-        new_list.append(True)
-    else:
-        new_list.append(False)
-        return new_list
+        if my_list[i] % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
