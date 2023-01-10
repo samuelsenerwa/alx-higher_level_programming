f56e9b0c4b945f55184aac560e1c0e18d94dbc6f
samuelsenerwa <samuelsenerwa@gmail.com>
2023-01-10 02:10:39 -0800

    added  task

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#define <stdlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -17,7 +17,7 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} listint_s;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
