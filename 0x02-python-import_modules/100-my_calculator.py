9a66292369d415247e51d4ebb081ad6e1acc94a2
samuelsenerwa <samuelsenerwa@gmail.com>
2023-01-12 07:12:42 -0800

    updated my calculator code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <ab>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
