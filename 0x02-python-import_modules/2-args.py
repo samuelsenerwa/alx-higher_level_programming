180ee8f290569f7b2d4869497e1f169771951e15
samuelsenerwa <samuelsenerwa@gmail.com>
2023-01-12 04:23:02 -0800

    updated argv code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the number of and lists of arguments."""
+    """Print the number of and list of arguments."""
     import sys
 
     count = len(sys.argv) - 1
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(count))
     for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}.format(i + 1, sys.argv[i + 1]))
