0a0e1d209ce8c75db328c58f3336b4819c01c8e9
samuelsenerwa <samuelsenerwa@gmail.com>
2023-01-23 17:19:00 -0800

    updated cde

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bn/python3
+
+def safe_print_integer(value):
+    """Print an integer with "{:d}"format.
+
+    Args:
+    calue (int): The integer to prunt.
+
+    Returns:
+    If a TypeError or ValueError occurs - False.
+    Otherwise - True.
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, valueError):
+        return (False)
