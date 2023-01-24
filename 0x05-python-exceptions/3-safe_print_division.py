f8c687ef0d0375f2111e80b473da7268959a2d5a
samuelsenerwa <samuelsenerwa@gmail.com>
2023-01-23 17:50:49 -0800

    updated code

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+
+def safe_print_division(a, b):
+    """Returns divison of a by b."""
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
