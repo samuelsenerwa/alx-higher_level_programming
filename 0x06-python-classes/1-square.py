35b95db02015e1058d1332de8fd85783f4255334
samuelsenerwa <samuelsenerwa@gmail.com>
2023-01-25 13:01:18 -0800

    added a task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a class Square."""
+
+
+class Sqaure:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+        size (int): The size of the new square.
+        """
+        self.__size = size
