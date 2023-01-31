1e8d7b554680aa7b4064b701df35bcc2a280c955
samuelsenerwa <samuelsenerwa@gmail.com>
2023-01-30 19:58:15 -0800

    added a task

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -65,8 +65,12 @@ class Rectangle:
                 rect.append("\n")
         return ("".join(rect))
 
-    def __repr_(self):
+    def __repr__(self):
         """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return (rect)
+
+    def __del__(self):
+        """Print a message for every deletion of Rectangle."""
+        print("Bye rectangle...")
