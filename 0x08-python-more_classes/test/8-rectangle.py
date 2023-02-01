5398599c67537d97cb42b1545329140613f21301
samuelsenerwa <samuelsenerwa@gmail.com>
2023-01-31 23:38:09 -0800

    addded a task

--- 0x08-python-more_classes/test/8-rectangle.py
+++ 0x08-python-more_classes/test/8-rectangle.py
@@ -60,8 +60,27 @@ class Rectangle:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Return the rectangle wth the greater area.
+
+        Args:
+        rect_1 (Rectangle): The first rectangle
+        rect_2 (Rectangle): The second Rectangle
+
+        Raises:
+        TypeError: If either of rect_1 or rect_2 is not a Rectangle
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() => rect_2.area():
+            return (rect_1)
+        return (rect_2)
+
     def __str__(self):
-        """Returns the printable representation of the Rectangle
+        """Return the printable representation of the Rectangle
         using the # character
         """
         if self.__width == 0 or self.__height == 0:
@@ -69,7 +88,7 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
