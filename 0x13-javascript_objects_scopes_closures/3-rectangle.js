a9d42b30faac81c07a87fda595da49877897f54a
samuelsenerwa <samuelsenerwa@gmail.com>
2023-04-12 13:38:30 -0700

    JS tasks

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,15 +7,15 @@ class Rectangle {
     }
   }
 
-	print() {
-		for (let i = 0; i < this.height; i++) {
-			let s = '';
-			for (let j = 0; j < this.width; j++) {
-				s += 'X';
-			}
-			console.log(s);
-		}
-	}
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
 }
 
 module.exports = Rectangle;
