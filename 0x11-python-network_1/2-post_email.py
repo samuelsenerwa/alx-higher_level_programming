606d59e692e7e685df641234c5bf84a1352a3e1f
samuelsenerwa <samuelsenerwa@gmail.com>
2023-05-02 06:49:25 -0700

    tasks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""
- sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)
+"""Sends a POST request to a given URL with a given email.
+Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
 import sys
 import urllib.parse
