2b593489d5267308dae0ed9a24fda3d608b9973f
samuelsenerwa <samuelsenerwa@gmail.com>
2023-05-23 01:05:39 -0700

    js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,12 @@
- Web Scrapping
+#!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function(err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
+	}
+});
